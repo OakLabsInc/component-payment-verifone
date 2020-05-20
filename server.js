209b5d2c42c85446fb7eb83c401d90c46132dadc
Richard Joffray <richard@joffray.com>
2020-05-20 13:24:58 -0700

    adding JSON response

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.post('/', function (req, res) {
       }
       // This callback is invoked once the child terminates
       // You'd want to check err/stderr as well!
-      console.log("Here is the complete output of the program: ");
       console.log("stdout: ", stdout)
       console.log("stderr: ", stderr)
+      res.json(JSON.parse(stdout))
   });
 
 })
