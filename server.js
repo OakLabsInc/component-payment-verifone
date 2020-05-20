d5148964fc632c3f6dab4f9f48ab91518e2cb000
Richard Joffray <rjoffray@richards-mbp.lan>
2020-05-19 18:19:58 -0700

    more logging

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.post('/', function (req, res) {
       // This callback is invoked once the child terminates
       // You'd want to check err/stderr as well!
       console.log("Here is the complete output of the program: ");
-      console.log(stdout)
+      console.log("stdout: ", stdout)
+      console.log("stderr: ", stderr)
   });
 
 })
